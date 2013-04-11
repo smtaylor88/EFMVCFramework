e93cd1c0467683b757efa4eeb5bb4887f927a48d
Steve Taylor <smtaylor_2001@yahoo.com>
2013-04-11 15:51:20 -0700

    Initial Loading of files

---
+++ EFMVC.Web/Controllers/CategoryController.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using EFMVC.Web.ViewModels;
+using EFMVC.Domain.Commands;
+using EFMVC.Core.Common;
+using EFMVC.Web.Core.Extensions;
+using EFMVC.CommandProcessor.Dispatcher;
+using EFMVC.Data.Repositories;
+using EFMVC.Web.Core.ActionFilters;
+using AutoMapper;
+using EFMVC.Model;
+using EFMVC.Web.Caching;
+namespace EFMVC.Web.Controllers
+{
+    [CompressResponse]
+    public class CategoryController : Controller
+    {
+        private readonly ICommandBus commandBus;
+        private readonly ICategoryRepository categoryRepository;
+        /************* The below commented code for Windows Azure Caching ****************
+        //----------------------------------------------------------------------------------
+        //private readonly ICacheProvider cache;
+        //public CategoryController(ICommandBus commandBus, ICategoryRepository categoryRepository, ICacheProvider cacheProvider)
+        //{
+        //    this.commandBus = commandBus;
+        //    this.categoryRepository = categoryRepository;
+        //    this.cache = cacheProvider;
+        //}
+         */
+        public CategoryController(ICommandBus commandBus, ICategoryRepository categoryRepository)
+        {
+            this.commandBus = commandBus;
+            this.categoryRepository = categoryRepository;           
+        }
+        public ActionResult Index()
+        {
+            //Get cached data from Cache if the data exist, 
+            //IEnumerable<Category> categories = null;
+            //var cachedCategories = cache.Get("categories");
+
+            //if (cachedCategories != null)
+            //{
+            //    categories = cachedCategories as IEnumerable<Category>;
+            //}
+            //else
+            //{
+            //    //Data from Database
+            //    categories = categoryRepository.GetAll().ToList();
+            //    if (categories != null)
+            //        cache.Put("categories", categories);
+            //}
+           var categories = categoryRepository.GetAll().ToList();
+            return View(categories);
+        }
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var category = categoryRepository.GetById(id);
+            var viewModel = Mapper.Map<Category, CategoryFormModel>(category);
+            return View(viewModel);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Save(CategoryFormModel form)
+        {
+            if (ModelState.IsValid)
+            {
+                var command = Mapper.Map<CategoryFormModel, CreateOrUpdateCategoryCommand>(form);
+                IEnumerable<ValidationResult> errors = commandBus.Validate(command);
+                ModelState.AddModelErrors(errors);
+                if (ModelState.IsValid)
+                {
+                    var result = commandBus.Submit(command);
+                    if (result.Success) {
+                        var categories = categoryRepository.GetAll().ToList();
+                        //cache.Put("categories", categories);
+                        return RedirectToAction("Index");
+                    }
+                }
+            }
+            //if fail
+            if (form.CategoryId == 0)
+                return View("Create", form);
+            else
+                return View("Edit", form);
+        }
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var command = new DeleteCategoryCommand { CategoryId = id };
+            var result = commandBus.Submit(command);
+            var categories = categoryRepository.GetAll();
+            //cache.Put("categories", categories);
+            return PartialView("_CategoryList", categories);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,23 @@
+EFMVC.Azure
+===========
+
+Windows Azure extensions for the EFMVC [http://efmvc.codeplex.com/] project 
+
+Windows Azure Caching Preview 
+-----------------------------
+
+Cache Worker Role Project - https://github.com/shijuvar/EFMVC.Azure/tree/master/EFMVC.CacheWorkerRole <br>
+Cache Client Project - https://github.com/shijuvar/EFMVC.Azure/tree/master/EFMVC.Web <br>
+Cache Providers - https://github.com/shijuvar/EFMVC.Azure/tree/master/EFMVC.Web/Caching <br>
+
+<b>Wiki Pages</b>
+
+* [How to configure Windows Azure Caching Preview] (https://github.com/shijuvar/EFMVC.Azure/wiki/How-to-configure-Windows-Azure-Caching)
+
+
+
+Upcoming Features
+-----------------
+1. AutoScaling Windows Azure instances.
+2. Windows Azure storage demos.
+3. Hybrid Cloud scenarios.
